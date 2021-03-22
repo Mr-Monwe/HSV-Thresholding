3f85a841888551c5849722a16dc4e54af613e8ae
Mr-Monwe <nnamdimonwe@gmail.com>
2021-03-21 23:44:02 -0400

    added ui

---
+++ src/depend.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'depend.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_mainWindow(object):
+    def setupUi(self, mainWindow):
+        mainWindow.setObjectName("mainWindow")
+        mainWindow.resize(720, 670)
+        mainWindow.setMinimumSize(QtCore.QSize(720, 670))
+        mainWindow.setMaximumSize(QtCore.QSize(720, 670))
+        mainWindow.setStyleSheet("background-color: rgb(46, 52, 54);")
+        self.centralwidget = QtWidgets.QWidget(mainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setObjectName("centralwidget")
+        self.div_line = QtWidgets.QFrame(self.centralwidget)
+        self.div_line.setGeometry(QtCore.QRect(120, 510, 471, 20))
+        self.div_line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.div_line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.div_line.setObjectName("div_line")
+        self.hue_horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.hue_horizontalSlider.setGeometry(QtCore.QRect(179, 540, 350, 20))
+        self.hue_horizontalSlider.setStyleSheet("")
+        self.hue_horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.hue_horizontalSlider.setObjectName("hue_horizontalSlider")
+        self.saturation_horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.saturation_horizontalSlider.setGeometry(QtCore.QRect(180, 580, 350, 16))
+        self.saturation_horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.saturation_horizontalSlider.setObjectName("saturation_horizontalSlider")
+        self.value_horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.value_horizontalSlider.setGeometry(QtCore.QRect(180, 620, 350, 16))
+        self.value_horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.value_horizontalSlider.setObjectName("value_horizontalSlider")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 10, 640, 480))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../../../../assets/cv.png"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("label")
+        self.hue_label = QtWidgets.QLabel(self.centralwidget)
+        self.hue_label.setGeometry(QtCore.QRect(70, 540, 28, 17))
+        self.hue_label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.hue_label.setObjectName("hue_label")
+        self.saturation_label = QtWidgets.QLabel(self.centralwidget)
+        self.saturation_label.setGeometry(QtCore.QRect(70, 580, 72, 17))
+        self.saturation_label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.saturation_label.setObjectName("saturation_label")
+        self.value_label = QtWidgets.QLabel(self.centralwidget)
+        self.value_label.setGeometry(QtCore.QRect(70, 620, 39, 17))
+        self.value_label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.value_label.setObjectName("value_label")
+        self.hue_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.hue_lineEdit.setGeometry(QtCore.QRect(570, 540, 81, 21))
+        self.hue_lineEdit.setStyleSheet("color: rgb(255, 255, 255);")
+        self.hue_lineEdit.setObjectName("hue_lineEdit")
+        self.saturation_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.saturation_lineEdit.setGeometry(QtCore.QRect(570, 580, 81, 21))
+        self.saturation_lineEdit.setStyleSheet("color: rgb(255, 255, 255);")
+        self.saturation_lineEdit.setObjectName("saturation_lineEdit")
+        self.value_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.value_lineEdit.setGeometry(QtCore.QRect(570, 620, 81, 21))
+        self.value_lineEdit.setStyleSheet("color: rgb(255, 255, 255);")
+        self.value_lineEdit.setObjectName("value_lineEdit")
+        mainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(mainWindow)
+        QtCore.QMetaObject.connectSlotsByName(mainWindow)
+
+    def retranslateUi(self, mainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        mainWindow.setWindowTitle(_translate("mainWindow", "MainWindow"))
+        self.hue_label.setText(_translate("mainWindow", "Hue"))
+        self.saturation_label.setText(_translate("mainWindow", "Saturation"))
+        self.value_label.setText(_translate("mainWindow", "Value"))
+        self.hue_lineEdit.setText(_translate("mainWindow", "0"))
+        self.saturation_lineEdit.setText(_translate("mainWindow", "0"))
+        self.value_lineEdit.setText(_translate("mainWindow", "0"))
---
+++ src/main.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import sys
+import platform
+from os import system, path
+from PyQt5 import QtCore, QtGui, QtWidgets
+# Main window UI File
+from depend import Ui_mainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self)
+        self.show()
+        #
+    #     # When checked connect the appropriate checkboxes to the corresponding function
+    #     self.ui.checkBox_p1.stateChanged.connect(self.p1)
+    #     # On toolbutton click execute the find_directory function
+    #     self.ui.toolButton.clicked.connect(self.find_directory)
+    #     # On append_button click execute the execute_append function
+    #     self.ui.append_button.clicked.connect(self.execute_append)
+    #     # On reset_button click execute the execute_reset function
+    #     self.ui.reset_button.clicked.connect(self.execute_reset)
+    #     # On execute_button click execute the execute_abort function
+    #     self.ui.abort_button.clicked.connect(self.execute_abort)
+
+    # # Function to append the first parameter to the file
+    # def p1(self, checked):
+    #     statement = "<launch> parameter 1 placeholder </launch>"
+    #     if checked:
+    #         self.ui.file_preview.append(statement)
+    #     else:
+    #         self.ui.file_preview.undo()
+
+    # # Function to append file to preview
+
+    # def execute_append(self):
+    #     # Select all the text in the file editor
+    #     self.ui.file_editor.selectAll()
+    #     # Cut all the selected text in the file editor
+    #     self.ui.file_editor.cut()
+    #     # Append an empty line in the file preview
+    #     self.ui.file_preview.append("")
+    #     # Move to the beginning of the next empty block
+    #     self.ui.file_preview.moveCursor(QtGui.QTextCursor.NextBlock)
+    #     # Paste all the copied text into the file preview
+    #     self.ui.file_preview.paste()
+    #     # Append an empty line in the file preview
+    #     self.ui.file_preview.append("")
+
+    # # Function to find the directory to save file
+    # def find_directory(self):
+    #     # Pop open file explorer so user can select the directory they want to use
+    #     directory = QtWidgets.QFileDialog.getExistingDirectory()
+    #     # Preview the directory in the File Path window
+    #     self.ui.text_directory_preview.setText(directory)
+    #     self.ui.text_directory_preview.home(True)
+    #     # On build_button click execute the execute_build function
+    #     self.ui.build_button.clicked.connect(self.execute_build)
+
+    # # Function to build the file and save it to the specified directory
+    # def execute_build(self):
+    #     # If filename is modified then allow the build to execute
+    #     if (self.ui.file_name_input.isModified() == True):
+    #         # Read the users input for File Name
+    #         fileName = self.ui.file_name_input.text()
+    #         # File Path where the file will be saved
+    #         fileDir = self.ui.text_directory_preview.text()
+    #         # Append the File Name to the File Path
+    #         completeName = path.join(fileDir, fileName)
+    #         # Create & open the writable output file
+    #         outFile = open(completeName, "w")
+    #         # Write the contents of the File Preview into the created file
+    #         outFile.write(str(self.ui.file_preview.toPlainText()))
+    #         # Close the file
+    #         outFile.close()
+    #         print("File Name: " + fileName)
+    #         print("Is Saved In Directory:" + fileDir)
+
+    # # Function to (Reset) the Config Manager
+    # def execute_reset(self):
+    #     # Uncheck all check boxes
+    #     self.ui.checkBox_p1.setChecked(False)
+    #     # Clear all the text in the file editor
+    #     self.ui.file_editor.clear()
+    #     # Clear all the text in the file preview
+    #     self.ui.file_preview.clear()
+    #     # Clear the filename being displayed
+    #     self.ui.file_name_input.clear()
+    #     # Clear the current directory being displayed
+    #     self.ui.text_directory_preview.clear()
+
+    # # Function to (Close) the Config Manager
+    # def execute_abort(self):
+    #     self.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
