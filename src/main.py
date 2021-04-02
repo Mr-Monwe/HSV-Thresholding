f1009984aa2f510b68a3e7cf3d3ec279c89e8b58
Mr-Monwe <nnamdimonwe@gmail.com>
2021-04-01 22:46:42 -0400

    finished ammending sliders

--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 # Main window UI File
 from depend import Ui_mainWindow
 
-CAMPATH = 0
-
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -20,20 +18,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.show()
         # Connect slider for Upper Hue parameter to its function
         self.ui.upper_hue_horizontalSlider.valueChanged.connect(self.adjustHueUpper)
+        # Connect upper hue line edit to slider function
+        self.ui.upper_hue_lineEdit.returnPressed.connect(self.setHueUpper)
         # Connect slider for Upper Saturation parameter to its function
         self.ui.upper_saturation_horizontalSlider.valueChanged.connect(self.adjustSaturationUpper)
+        # Connect upper saturation line edit to slider function
+        self.ui.upper_saturation_lineEdit.returnPressed.connect(self.setSaturationUpper)
         # Connect slider for Upper Value parameter to its function
         self.ui.upper_value_horizontalSlider.valueChanged.connect(self.adjustValueUpper)
+        # Connect upper value line edit to slider function
+        self.ui.upper_value_lineEdit.returnPressed.connect(self.setValueUpper)
         # Connect slider for Lower Hue parameter to its function
         self.ui.lower_hue_horizontalSlider.valueChanged.connect(self.adjustHueLower)
+        # Connect lower hue line edit to slider function
+        self.ui.lower_hue_lineEdit.returnPressed.connect(self.setHueLower)
         # Connect slider for Lower Saturation parameter to its function
         self.ui.lower_saturation_horizontalSlider.valueChanged.connect(self.adjustSaturationLower)
+        # Connect lower saturation line edit to slider function
+        self.ui.lower_saturation_lineEdit.returnPressed.connect(self.setSaturationLower)
         # Connect slider for Lower Value parameter to its function
         self.ui.lower_value_horizontalSlider.valueChanged.connect(self.adjustValueLower)
-        # Connect the test push button to trigger the camera path
-        self.ui.test_pushButton.clicked.connect(self.parseCameraPath)
-        # Connect the reset push button to reset the camera path
-        self.ui.reset_pushButton.clicked.connect(self.resetCameraPath)
+        # Connect lower value line edit to slider function
+        self.ui.lower_value_lineEdit.returnPressed.connect(self.setValueLower)
         # create the video capture thread
         self.thread = VideoThread()
         # connect its signal to the update_image slot
@@ -41,55 +47,72 @@ class MainWindow(QtWidgets.QMainWindow):
         # start the thread
         self.thread.start()
 
-    # Function to Adjust hue slider
+    # Function to adjust upper hue slider
     def adjustHueUpper(self, value):
         self.ui.upper_hue_lineEdit.setText(str(value))
 
-    # Function to Adjust hue slider
+    # Function to force upper hue slider to given int
+    def setHueUpper(self):
+        val = int(self.ui.upper_hue_lineEdit.text())
+        self.ui.upper_hue_horizontalSlider.setValue(val)
+
+    # Function to adjust upper saturation slider
     def adjustSaturationUpper(self, value):
         self.ui.upper_saturation_lineEdit.setText(str(value))
 
-    # Function to Adjust hue slider
+    # Function to force upper saturation slider to given int
+    def setSaturationUpper(self):
+        val = int(self.ui.upper_saturation_lineEdit.text())
+        self.ui.upper_saturation_horizontalSlider.setValue(val)
+
+    # Function to adjust upper value slider
     def adjustValueUpper(self, value):
         self.ui.upper_value_lineEdit.setText(str(value))
 
-    # Function to Adjust hue slider
+    # Function to force upper value slider to given int
+    def setValueUpper(self):
+        val = int(self.ui.upper_value_lineEdit.text())
+        self.ui.upper_value_horizontalSlider.setValue(val)
+
+    # Function to adjust lower hue slider
     def adjustHueLower(self, value):
         self.ui.lower_hue_lineEdit.setText(str(value))
 
-    # Function to Adjust hue slider
+    # Function to force lower hue slider to given int
+    def setHueLower(self):
+        val = int(self.ui.lower_hue_lineEdit.text())
+        self.ui.lower_hue_horizontalSlider.setValue(val)
+
+    # Function to adjust lower saturation slider
     def adjustSaturationLower(self, value):
         self.ui.lower_saturation_lineEdit.setText(str(value))
 
-    # Function to Adjust hue slider
+    # Function to force lower saturation slider to given int
+    def setSaturationLower(self):
+        val = int(self.ui.lower_saturation_lineEdit.text())
+        self.ui.lower_saturation_horizontalSlider.setValue(val)
+
+    # Function to adjust lower value slider
     def adjustValueLower(self, value):
         self.ui.lower_value_lineEdit.setText(str(value))
+
+    # Function to force lower value slider to given int
+    def setValueLower(self):
+        val = int(self.ui.lower_value_lineEdit.text())
+        self.ui.lower_value_horizontalSlider.setValue(val)
 
     def closeEvent(self, event):
         self.thread.stop()
         event.accept()
 
-    # Function to get camera path set by the user
-    def parseCameraPath(self):
-        # print(self.ui.camera_path_lineEdit.text())
-        global CAMPATH
-        CAMPATH = int(self.ui.camera_path_lineEdit.text())
-        # return CAMPATH
-
-    # Function to reset camera path to 0
-    def resetCameraPath(self):
-        self.ui.camera_path_lineEdit.setText("0")
-        global CAMPATH
-        CAMPATH = 0
-
+    # Updates the image_label with a new OpenCV image
     @QtCore.pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
-        """Updates the image_label with a new opencv image"""
         qt_img = self.convert_cv_qt(cv_img)
         self.ui.label.setPixmap(qt_img)
 
+    # Function to convert OpenCV output to QPixmap
     def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         # Convert BGR to HSV
         hsv = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
@@ -106,7 +129,6 @@ class MainWindow(QtWidgets.QMainWindow):
         mask = cv2.inRange(hsv, lower_bound, upper_bound)
         # Bitwise-AND mask and original image
         res = cv2.bitwise_and(rgb_image, rgb_image, mask=mask)
-        #rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         h, w, ch = res.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(res.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
@@ -122,19 +144,17 @@ class VideoThread(QtCore.QThread):
         self._run_flag = True
 
     def run(self):
-        # capture from web cam 
-        global CAMPATH
-        cap = cv2.VideoCapture(CAMPATH)  
+        # Set camera path and capture from web cam
+        cap = cv2.VideoCapture(2)
         while self._run_flag:
             ret, cv_img = cap.read()
-            print (CAMPATH)
             if ret:
                 self.change_pixmap_signal.emit(cv_img)
-        # shut down capture system
+        # Terminate capture
         cap.release()
 
     def stop(self):
-        """Sets run flag to False and waits for thread to finish"""
+        # Sets run flag to False and waits for thread to finish
         self._run_flag = False
         self.wait()
 
@@ -144,7 +164,3 @@ if __name__ == "__main__":
     ui = MainWindow()
     ui.show()
     sys.exit(app.exec_())
-
-
-# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
-# https://docs.wpilib.org/en/stable/docs/software/vision-processing/wpilibpi/image-thresholding.html
